033d59f5f001e003afc90abf18dbe07faad96841
Nuno Brito <brito.n@outlook.pt>
2024-03-23 09:38:25 +0000

    Updated tp1/vector_stat_proc_funcs.h.

---
+++ tp1/vector_stat_proc_funcs.h
@@ -0,0 +1,4 @@
+int vector_get_in_range(int v[], int v_sz, int sv[], int min, int max, int n_processes);
+int get_random(int min, int max);
+void vector_init_rand(int v[], long dim, int min, int max);
+
