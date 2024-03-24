6ede9d0c4cd54e26ca70284a52716d95d00e000c
nmbrito <brito.n@outlook.pt>
2024-03-24 23:15:49 +0000

    Updated tp1/vectorstat/vector_stat_proc_child.c.

--- tp1/vectorstat/vector_stat_proc_child.c
+++ tp1/vectorstat/vector_stat_proc_child.c
@@ -2,7 +2,19 @@
 
 int processoFilho()
 {
-    int contagem;
+
+    pid_t ramal = fork();
+
+    if(ramal < FILHO)
+    {
+        // Falha na criação do ramal
+        perror("Erro na criação do processo filho.");
+        return -1;
+    }
+    else if(ramal == FILHO)
+    {
+        return processoFilho(); //TODO
+    }
 
     close(tuboFilho[LEITURA]);  // Fecha o canal de leitura (STDIN_FILENO = 0)
                                 // Vai-se tirar partido do aspeto de clonagem para receber a leitura do vetor
