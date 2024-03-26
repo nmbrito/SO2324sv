bc4eec5eaa9b12138e5f1070bb888f667a83398a
Nuno Brito <brito.n@outlook.pt>
2024-03-26 12:48:52 +0000

    Updated tp1/vectorstat/child.h.

---
+++ tp1/vectorstat/child.h
@@ -0,0 +1,18 @@
+#ifndef CHILD_H
+#define CHILD_H
+// MACROS ------------------------------------------------------------------- //
+// Provided (apoioTP1)
+#define LOWER_LIMIT             0
+#define UPPER_LIMIT             100
+
+// Custom macros
+#define CHILD                   0
+#define READ                    STDIN_FILENO
+#define WRITE                   STDOUT_FILENO
+#define PIPE_UNICHANNEL         2
+#define PIPE_BICHANNEL          4
+#define ARGUMENT_COUNT_ERROR    -1
+
+int childlabor(int **V, int **v2)
+
+#endif
