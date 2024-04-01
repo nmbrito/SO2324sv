9a8adbb72e7ef83183351c26d65b3a51eeb3f934
Nuno Brito <brito.n@outlook.pt>
2024-04-01 12:25:09 +0100

    Updated tp1/vectorstat/utilities.c.

--- tp1/vectorstat/utilities.c
+++ tp1/vectorstat/utilities.c
@@ -1,28 +1,38 @@
+// HEADERS ------------------------------------------------------------------ // {{{1
+// System
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+// Custom
 #include "utilities.h"
-#include "child.h"
 
-// ASSIGNMENT WORK----------------------------------------------------------- //
+// -------------------------------------------------------------------------- // 1}}}
+
+// ASSIGNMENT WORK----------------------------------------------------------- // {{{1
 int vector_get_in_range(int v[], int v_sz, int sv[], int min, int max, int n_processes)
 {
     int slices = v_sz / n_processes;        // Number of elements to assign
     int sliceLeftover = v_sz % n_processes; // Odd number residue
 
-    // Pipe array
-    int *pipesfds = createArrays(&pipesfds, n_processes * PIPE_UNICHANNEL);
+    int addLeftover = 0;                    // Adds value to v[] address
+    int moveLeftover = 0;                   // Forward v[] address
 
-    // Testing
-    for(int ongoingProcesses = 0; ongoingProcesses < n_processes; ongoingProcesses++, sliceLeftover--)
+    long svCount[1] = 0;                    // Number of items written in sv
+
+    // Pipe array
+    int *pipesFDS = createArrays(&pipesFDS, (n_processes * PIPE_UNICHANNEL));
+    int *pipesFDSAddress = pipesFDS;
+
+    for(int ongoingProcesses = 0; ongoingProcesses < n_processes; ongoingProcesses++)
     {
         if(sliceLeftover > 0)
         {
             addLeftover = 0;
             moveLeftover = 1;
+            sliceLeftover--;
         }
         else
         {
@@ -30,21 +40,86 @@ int vector_get_in_range(int v[], int v_sz, int sv[], int min, int max, int n_pro
             moveLeftover = 0;
         }
 
-        // Send *v to *(v+slices+addLeftover)
-        childlabor();
-        v=v+(slices+moveLeftover);      // Move to next element address
-        pipesfds += PIPE_UNICHANNEL;    // Move to next pipe address
+        // Pipe creation
+        pipe(pipesFDS);
+
+        // Fork creation
+        pid_t forker = fork();
+        if(forker < CHILD)  // Child error
+        {
+            perror("Error forking");
+            return -1;
+        }
+        else if(forker == CHILD)    // Child work
+        {
+            close(pipesFDS[READ]);  // Child only writes to parent
+
+            // *v to *(v+slices+addLeftover) // Valores, sem * = endereço
+            long count = 0;
+            for (long i = 0; i < (slices + addLeftover); i++) //Origina: for (long i = 0; i < v_sz; i++)
+            {
+                if (v[i] >= min && v[i] <= max)
+                {
+                    sv[count++] = v[i];
+                    /**///sv[svCount[0]++] = v[i];
+                }
+            }        
+
+            // Pipe write
+            /**/write(pipesFDS[WRITE], svCount, (sizeof(long) * count));
+            /**///write(pipesFDS[WRITE], svCount, (sizeof(long) * svCount[0]));
+            write(pipesFDS[WRITE], sv, (sizeof(int) * count));
+            /**///write(pipesFDS[WRITE], sv, (sizeof(int) * svCount[0]));
+
+            // Pipe close
+            close(pipesFDS[WRITE]);
+
+            free(v);
+            free(sv);
+            free(pipesFDS);
+
+            return 0;
+        }
+        else    // Parent work
+        {
+            v=v+(slices+moveLeftover);      // Move to next element address
+            pipesFDS += PIPE_UNICHANNEL;    // Move to next pipe address
+
+        }
     }
+
+    /**/long countnumber = 0;
+
+    pipesFDS = pipesFDSAddress;  // Recover original address
+    sv = realloc(sv, (sizeof(int) * v_sz));
 
     for(int closeProcesses = 0; closeProcesses < n_processes; closeProcesses++)
     {
-        wait(NULL); // Parent waits for children
+        close(pipesFDS[WRITE]);
+
+        // Se isto funcionar, escuso de fazer a deteção do -1
+        /**/read(pipesFDS[READ], svCount, sizeof(long) * 1);
+        read(pipesFDS[READ], arraySubValues, sizeof(int) * svCount[0]);
+
+        /**/countnumber += svCount[0];
+        close(pipesFDS[READ]);
+        pipesFDS += 1;
+
+    }
+
+    sv = realloc(sv, (sizeof(int) * countnumber));
+
+    // Wait for children
+    for(int closeProcesses = 0; closeProcesses < n_processes; closeProcesses++)
+    {
+        wait(NULL);
     }
 
     return countnumber;
 }
+// -------------------------------------------------------------------------- // 1}}}
 
-// CUSTOM FUNCTIONS --------------------------------------------------------- //
+// CUSTOM FUNCTIONS --------------------------------------------------------- // {{{1
 int createArrays(int **vector, long vsize)
 {
     printf("Initializing a vector of %ld bytes\n", vsize);
@@ -58,8 +133,9 @@ int createArrays(int **vector, long vsize)
     
     return *vector;
 }
+// -------------------------------------------------------------------------- // 1}}}
 
-// APOIOTP1 FUNCTIONS-------------------------------------------------------- //
+// APOIOTP1 FUNCTIONS-------------------------------------------------------- // {{{1
 // Random number generator
 int get_random(int min, int max)
 {
