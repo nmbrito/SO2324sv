c7d7cfd2c7ab910f45c29da9ad4f0559dbd7808f
Nuno Brito <brito.n@outlook.pt>
2024-04-06 01:03:07 +0100

    Updated tp1/utilities.h.

--- tp1/utilities.h
+++ tp1/utilities.h
@@ -1,25 +1,27 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
-// MACROS ------------------------------------------------------------------- //
+// MACROS ------------------------------------------------------------------- // {{{1
 // Provided (apoioTP1)
 #define LOWER_LIMIT             0
 #define UPPER_LIMIT             100
 #define MAXIMUM_LIMIT           200
-
 // Custom macros
 #define CHILD                   0
-#define READ                    STDIN_FILENO
-#define WRITE                   STDOUT_FILENO
+#define READ                    0
+#define WRITE                   1
 #define PIPE_UNICHANNEL         2
-#define PIPE_BICHANNEL          4
 #define ERROR_RETURN            -1
 #define CHILD_RETURN_SUCCESS    -2
+// -------------------------------------------------------------------------- // 1}}}
 
+// FUNCTIONS ---------------------------------------------------------------- // {{{1
 int vector_get_in_range(int v[], int v_sz, int sv[], int min, int max, int n_processes);
-int *createIntArrays(int **array, long arrayDim);
 int get_random(int min, int max);
-void vector_init_rand(int v[], long dim, int min, int max);
 int vector_get_in_range_seq(int v[], int v_sz, int sv[], int min, int max);
-void codeEval();
+//int *createIntArrays(int **array, long arrayDim);
+void vector_init_rand(int v[], long dim, int min, int max);
+//void codeEval();
+// -------------------------------------------------------------------------- // 1}}}
 
 #endif
+
