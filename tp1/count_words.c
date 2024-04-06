fd897d0bc4f0f84be8be9fe011883c453559e624
Nuno Brito <brito.n@outlook.pt>
2024-04-06 01:03:07 +0100

    Updated tp1/count_words.c.

--- tp1/count_words.c
+++ tp1/count_words.c
@@ -1,31 +1,16 @@
-/*
- * Realize um programa cpu_stress com o objetivo de simular uma
- * carga de processamento no(s) CPU(s) disponíveis no sistema
- * através da criação de múltiplos processos.
- * 
- * Este programa recebe pelos argumentos da linha de comando o
- * número de processos auxiliares (processos filho) a serem criados.
- * Cada um dos processos auxiliares executa a função
- * process_work. Na Figura 1 ilustra-se a utilização desta função
- * num programa sequencial.
- * 
- * O processo pai só deve terminar depois de garantir que todos os
- * processos filho terminaram a sua execução.
- * Utilize os programas htop, top e ps num terminal para acompanhar
- * a execução do seu programa.
- */
-
-#include <stdio.h>
+// HEADERS ------------------------------------------------------------------ // {{{1
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <math.h>
-
-// Forks and stuff
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+// -------------------------------------------------------------------------- // 1}}}
 
+// MACROS ------------------------------------------------------------------- // {{{1
 #define CHILD 0
+// MACROS ------------------------------------------------------------------- // 1}}}
 
 int main(int argc, char *argv[])
 {
@@ -36,10 +21,11 @@ int main(int argc, char *argv[])
     }
 
     char *countArguments[4];
-    countArguments[0]=strdup("wc"); // program: wc
-    countArguments[1]=strdup("-w"); // arguments: -w
-    countArguments[2]=argv[1];      // terminal argument
-    countArguments[3]=NULL;         // mark end of array
+
+    countArguments[0] = strdup("wc");   // program: wc
+    countArguments[1] = strdup("-w");   // arguments: -w
+    countArguments[2] = argv[1];        // terminal argument
+    countArguments[3] = NULL;           // mark end of array
 
     execvp(countArguments[0], countArguments);
 
