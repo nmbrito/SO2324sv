e54e711ce27775d0a96b4f26bac6e5ec64773b43
nmbrito <brito.n@outlook.pt>
2024-05-04 12:14:45 +0100

    Updated tp2/src/vector_stat_client_utils.h.

--- tp2/src/vector_stat_client_utils.h
+++ tp2/src/vector_stat_client_utils.h
@@ -1,6 +1,6 @@
 #ifndef VECTOR_STAT_PROC_UTILS_H
 #define VECTOR_STAT_PROC_UTILS_H
-// MACROS ------------------------------------------------------------------- // {{{1
+// MACROS ------------------------------------------------------------------- //
 // Provided (apoioTP1)
 #define LOWER_LIMIT             0
 #define UPPER_LIMIT             100
@@ -8,10 +8,5 @@
 #define CHILD                   0
 #define PIPE_UNICHANNEL         2
 #define MAXIMUM_LIMIT           200
-// -------------------------------------------------------------------------- // 1}}}
+// -------------------------------------------------------------------------- //
 
-int tcp_socket_server_init (int serverPort);
-int tcp_socket_server_accept (int serverSocket);
-int tcp_socket_client_init (const char *host, int port);
-int un_socket_server_init (const char *serverEndPoint); int un_socket_server_accept (int serverSocket);
-int un_socket_client_init (const char *serverEndPoint);
