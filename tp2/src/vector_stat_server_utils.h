09621bf9c92335202317802ee910315f72672fc4
nmbrito <brito.n@outlook.pt>
2024-05-04 10:51:42 +0100

    Updated tp2/src/vector_stat_server_utils.h.

---
+++ tp2/src/vector_stat_server_utils.h
@@ -0,0 +1,13 @@
+#ifndef VECTOR_STAT_SERVER_UTILS_H
+#define VECTOR_STAT_SERVER_UTILS_H
+// MACROS ------------------------------------------------------------------- // {{{1
+// Provided (apoioTP1)
+#define LOWER_LIMIT             0
+#define UPPER_LIMIT             100
+// Custom macros
+#define CHILD                   0
+#define PIPE_UNICHANNEL         2
+#define MAXIMUM_LIMIT           200
+// -------------------------------------------------------------------------- // 1}}}
+
+#endif
