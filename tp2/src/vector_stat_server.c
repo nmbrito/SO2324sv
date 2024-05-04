b4023021884a9e7924e7f05289394703c864906d
nmbrito <brito.n@outlook.pt>
2024-05-04 12:14:45 +0100

    Updated tp2/src/vector_stat_server.c.

--- tp2/src/vector_stat_server.c
+++ tp2/src/vector_stat_server.c
@@ -1,3 +1,18 @@
+// ./vector_stat_server <processing type>
+// -p -> multiple processes
+// -t -> multiple threads
+
+/*
+ * Passos:
+ *  Definir socket TCP
+ *  Servidor processa o vetor usando as funções anteriores
+ *  O servidor deve ser estruturado de forma concorrente usando uma tarefa dedicada a cada ligação com um cliente.
+ *
+ *  Definir socket UNIX
+ *  Um processo, múltiplas tarefas
+ *
+ *  Funcionalidade multiplas tarefas / multiplos processos
+ */
 // HEADERS ------------------------------------------------------------------ //
 // System
 #include <stdlib.h>
@@ -7,17 +22,21 @@
 #include <sys/wait.h>
 
 #include "vector_stat_proc_utils.h"
+#include "vector_stat_server_utils.h"
 // -------------------------------------------------------------------------- //
 
 // MAIN --------------------------------------------------------------------- //
 // argv[0] -> program name
-// argv[1] -> array dimension
-// argv[2] -> number of processes
+// argv[1] -> processing type
 int main(int argc, char *argv[])
 {
     // Check arguments
-    if(argc != 3)
+    if(argc != 2)
     {
-        printf("Syntax wrong. Usage: $ ./vector_stat_proc <vector dimension> <number threads>");
+        printf("Syntax wrong. Usage: $ ./vector_stat_server <processing type> ");
         return -1;
     }
+
+
+    return 0;
+}
