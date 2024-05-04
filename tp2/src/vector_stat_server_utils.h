a623251494b713d774e60bb828d961b76d1434ba
nmbrito <brito.n@outlook.pt>
2024-05-04 12:14:45 +0100

    Updated tp2/src/vector_stat_server_utils.h.

--- tp2/src/vector_stat_server_utils.h
+++ tp2/src/vector_stat_server_utils.h
@@ -10,4 +10,21 @@
 #define MAXIMUM_LIMIT           200
 // -------------------------------------------------------------------------- // 1}}}
 
+int tcp_socket_server_init (int serverPort);                // From iohelpers
+int tcp_socket_server_accept (int serverSocket);            // From iohelpers
+int tcp_socket_client_init (const char *host, int port);    // From iohelpers
+
+int un_socket_server_init (const char *serverEndPoint);
+int un_socket_server_accept (int serverSocket);
+int un_socket_client_init (const char *serverEndPoint);
+
+// From SO2324sv iohelpers files
+int readn(int fd, void * buf, size_t count);
+int writen(int fd, const void * buf, size_t count); 
+int tcp_socket_create(char* ip, int port);
+int tcp_socket_accept(int socketFd);
+int tcp_socket_connect(char* ip, int port);
+void fatal(char* msg);
+
+
 #endif
