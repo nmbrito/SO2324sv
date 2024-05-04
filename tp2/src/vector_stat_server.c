f688b2d0c7d3e92080bdd2ab26c3d7efcdab1b5b
nmbrito <brito.n@outlook.pt>
2024-05-04 10:51:42 +0100

    Updated tp2/src/vector_stat_server.c.

---
+++ tp2/src/vector_stat_server.c
@@ -0,0 +1,23 @@
+// HEADERS ------------------------------------------------------------------ //
+// System
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "vector_stat_proc_utils.h"
+// -------------------------------------------------------------------------- //
+
+// MAIN --------------------------------------------------------------------- //
+// argv[0] -> program name
+// argv[1] -> array dimension
+// argv[2] -> number of processes
+int main(int argc, char *argv[])
+{
+    // Check arguments
+    if(argc != 3)
+    {
+        printf("Syntax wrong. Usage: $ ./vector_stat_proc <vector dimension> <number threads>");
+        return -1;
+    }
