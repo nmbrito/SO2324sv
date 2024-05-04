e027a4f62a44ef4cf7bfc0110783c02ab685b363
nmbrito <brito.n@outlook.pt>
2024-05-04 10:51:42 +0100

    Updated tp2/src/vector_stat_server_utils.c.

---
+++ tp2/src/vector_stat_server_utils.c
@@ -0,0 +1,10 @@
+// System
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "vector_stat_proc_utils.h"
+#include "vector_stat_proc_utils.h"
+// ------------------------------------------------------------------------------------- //
