3845ab8405567f00573a9a8beeceb5b1516de58e
nmbrito <brito.n@outlook.pt>
2024-05-04 19:38:51 +0100

    Updated tp2/src/vector_stat_client.c.

--- tp2/src/vector_stat_client.c
+++ tp2/src/vector_stat_client.c
@@ -1,3 +1,12 @@
+// ./vector_stat_client <vector dimension> <number processes / threads>
+
+// HEADERS ------------------------------------------------------------------ //
+// System
+#include <stdlib.h>
+
+#include <vector_stat_proc_utils.h>
+// -------------------------------------------------------------------------- //
+
 // MAIN --------------------------------------------------------------------- //
 // argv[0] -> program name
 // argv[1] -> array dimension
@@ -7,6 +16,34 @@ int main(int argc, char *argv[])
     // Check arguments
     if(argc != 3)
     {
-        printf("Syntax wrong. Usage: $ ./vector_stat_proc <vector dimension> <number threads>");
+        printf("Syntax wrong. Usage: $ ./vector_stat_proc <vector dimension> <number processes / threads>");
         return -1;
     }
+
+    long arraySize      = atoi(argv[1]);
+    int numberThreads   = atoi(argv[2]);
+
+    // Arrays
+    int *arrayValues = (int *) malloc(sizeof(int) * arraySize);
+    if(arrayValues == NULL) return -1;
+
+    int *arraySubValues = (int *) malloc(sizeof(int) * arraySize);
+    if(arraySubValues == NULL) return -1;
+
+    // Initiate array with random numbers
+    vector_init_rand(arrayValues, arraySize, LOWER_LIMIT, MAXIMUM_LIMIT);
+
+    // Send to server
+        // Call main function
+        int count = vector_get_in_range_with_threads(arrayValues, arraySize, arraySubValues, LOWER_LIMIT, UPPER_LIMIT, numberThreads);
+    // --------------
+
+    printf("Counted %d valid numbers", count);
+
+    // Free arrays
+    free(arrayValues);
+    free(arraySubValues);
+
+    return 0;
+}
+// -------------------------------------------------------------------------- //
