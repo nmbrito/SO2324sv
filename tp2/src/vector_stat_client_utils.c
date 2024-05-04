4e0e01e7034bbfe620010b550be061036518ddba
nmbrito <brito.n@outlook.pt>
2024-05-04 10:51:41 +0100

    Updated tp2/src/vector_stat_client_utils.c.

---
+++ tp2/src/vector_stat_client_utils.c
@@ -0,0 +1,26 @@
+
+// HEADERS ------------------------------------------------------------------ //
+// System
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "vector_stat_proc_utils.h"
+// -------------------------------------------------------------------------- //
+
+int tcp_socket_server_init (int serverPort)
+{
+
+}
+
+int tcp_socket_server_accept (int serverSocket);
+{
+
+}
+
+int tcp_socket_client_init (const char *host, int port);
+{
+
+}
